ecf67b5f97dfdb4127684da42dc7c6674b5c2909
Daniel Pimentel <you@example.com>
2018-06-25 18:17:00 -0400

    messed up file names

---
+++ src/Chatroom.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import Message from './Message.js';
+
+class Chatroom extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            chats : [{
+                username : 'Daniel Pimentel',
+                content : 'Sup Dawgs',
+                img : 'http://i.imgur.com/Tj5DGiO.jpg',
+            }, 
+            {
+                username : 'Daniel Pimentel',
+                content : 'Sup Dawgs',
+                img : 'http://i.imgur.com/Tj5DGiO.jpg',
+
+            },
+            {
+                username : 'Daniel Pimentel',
+                content : 'Sup Dawgs',
+                img : 'http://i.imgur.com/Tj5DGiO.jpg',
+            },
+        ]
+    };
+    this.submitMessage = this.submitMessage.bind(this);
+        
+        
+        
+        }
+        componentDidMount(){
+            this.scrollToBot();
+        }
+
+        componentDidUpdate(){
+            this.scrollToBot();
+        }
+
+    scrollToBot(){
+        ReactDOM.findDOMNode(this.refs.chats).scrollTop = ReactDOM.findDOMNode(this.refs.chats).scrollHeight;
+
+    }
+    submitMessage(e){
+        e.preventDefault();
+        this.setState({
+            chats : this.state.chats.concat([{
+                username : 'Daniel Pimentel',
+                content : <p>{ReactDOM.findDOMNode(this.refs.msg).value}</p>,
+                img : 'http://i.imgur.com/Tj5DGiO.jpg'
+            }])
+        }, ()=> 
+        {
+            ReactDOM.findDOMNode(this.refs.msg).value = '';
+        });
+    }
+    render(){
+        const username = 'Daniel Pimentel';
+        const { chats } = this.state; 
+
+        return(
+            <div className ="chatroom">
+            <h3> Chilltime</h3>
+            <ul className = "chats" ref="chats">
+            {
+                chats.map((chat)=>
+                <Message chat={chat} user={username} />
+            )
+            }
+            </ul>
+            <form className = 'input' onSubmit={(e)=> this.submitMessage(e)}>
+             <input type = 'text' ref ='msg' />
+             <input type = 'submit' value = 'Submit' />
+             </form>
+             </div>
+            );
+    }
+
+
+}
+export default Chatroom
+
--- src/Message.js
+++ src/Message.js
@@ -1,63 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-import Message from './Message.js';
 
-class Chatroom extends React.Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            chats : [{
-                username : 'Daniel Pimentel',
-                content : 'Sup Dawgs',
-                img : 'http://i.imgur.com/Tj5DGiO.jpg',
-            }, 
-            {
-                username : 'Daniel Pimentel',
-                content : 'Sup Dawgs',
-                img : 'http://i.imgur.com/Tj5DGiO.jpg',
-
-            },
-            {
-                username : 'Daniel Pimentel',
-                content : 'Sup Dawgs',
-                img : 'http://i.imgur.com/Tj5DGiO.jpg',
-            },
-        ]
-    };
-    this.submitMessage = this.submitMessage.bind(this);
-        
-        
-        
+const Message = ({chat, user}) => (
+    <li className={`chat ${user === chat.username ? "right" : "left"}`}>
+        {user !== chat.username
+            && <img src={chat.img} alt={`${chat.username}'s profile pic`} />
         }
-        componentDidMount(){
-            this.scrollToBot();
-        }
+        {chat.content}
+    </li>
+);
 
-        componentDidUpdate(){
-            this.scrollToBot();
-        }
-
-    scrollToBot(){
-        ReactDOM.findDOMNode(this.refs.chats).scrollTop = ReactDOM.findDOMNode(this.refs.chats).scrollHeight;
-
-    }
-    submitMessage(e){
-        e.preventDefault();
-        this.setState({
-
-
-            
-        })
-    }
-
-
-
-
-
-
-
-
-    }
-
+export default Message;
